4db652316da422b2a6f383d097a7091ed136a17a
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-01-10 17:06:22 +0530

    Initial commit - React projects

--- enrollment/src/pages/UpdateStudent.jsx
+++ enrollment/src/pages/UpdateStudent.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const UpdateStudent = () => {
   return (
-    <div>UpdateStudent</div>
+    <>
+    <center><h2>Add Student</h2></center>
+    <center>
+    <form action="" >
+        <input type="text" placeholder='Enter Name' required value={name} onChange={(e)=>{
+            setname(e.target.value)}} /><br />
+        <input type="text" placeholder='Enter Email' required value={email} onChange={(e)=>{setemail(e.target.value)}}/><br />
+        <input type="text" placeholder='Enter Department' required value={department} onChange={(e)=>{setdepartment(e.target.value)}}/><br />
+        <input type="text" placeholder='Enter Course' required value={course} onChange={(e)=>{setcourse(e.target.value)}} /><br />
+        <button>Add Student </button>
+    </form>
+    </center>
+    </>
   )
 }
 

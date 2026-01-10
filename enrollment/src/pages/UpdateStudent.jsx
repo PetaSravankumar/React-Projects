e7a8af32d3127a23954aa4a7f5e0423c36cec51f
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-01-10 17:36:59 +0530

    Initial commit - React projects

--- enrollment/src/pages/UpdateStudent.jsx
+++ enrollment/src/pages/UpdateStudent.jsx
@@ -1,13 +1,15 @@
 import React,{useEffect, useState} from 'react'
 import axios from 'axios'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 const UpdateStudent = () => {
     const [name,setname]=useState("")
     const [email,setemail]=useState("")
     const [department,setdepartment]=useState("")
     const [course,setcourse]=useState("")
+    let navigate=useNavigate()
 
-    let {id}=useParams
+    let {id}=useParams()
 
     useEffect(()=>{
         axios.get(`http://localhost:5000/users/${id}`)
@@ -16,14 +18,26 @@ const UpdateStudent = () => {
             setemail(x.data.email)
             setdepartment(x.data.department)
             setcourse(x.data.course)
+
         })
         .catch(err=>console.log(err)
         )
     },[])
 
+    function handelData(e){
+        e.preventDefault()
+        const data={name,email,department,course}
+        axios.put(`http://localhost:5000/users/${id}`)
+        .then(()=>{
+            navigate("/viewstudent")
+        })
+        .catch(err=>console.log(err)
+        )
+    }
+
   return (
     <>
-    <center><h2>Add Student</h2></center>
+    <center><h2>Update Student</h2></center>
     <center>
     <form action="" >
         <input type="text" placeholder='Enter Name' required value={name} onChange={(e)=>{

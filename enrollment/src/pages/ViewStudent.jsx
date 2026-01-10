844d7b5566f30618accf80a5cc506ecc1826a275
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-01-10 17:45:14 +0530

    Initial commit - React projects

--- enrollment/src/pages/ViewStudent.jsx
+++ enrollment/src/pages/ViewStudent.jsx
@@ -8,6 +8,7 @@ const ViewStudent = () => {
         axios.get("http://localhost:5000/users")
         .then(x=>setUser(x.data))
         .catch(err=>console.log(err))
+
     }
     useEffect(()=>{
         fetchData()
@@ -16,18 +17,26 @@ const ViewStudent = () => {
     function handelUpdate(id){
         navigate(`/updatestudent/${id}`)
     }
+    function handelDelete(id){
+        axios.delete(`http://localhost:5000/users/${id}`)
+        .then(()=>{
+            fetchData()
+        })
+        .catch(err=>console.log(err)
+        )
+    }
 
   return (
     <>
     <center><h1>View Student</h1></center>
     {users.map((x)=>{
-        return <div>
+        return <div key={x.id}>
             <h2>Name :{x.name}</h2>
             <h2>Email :{x.email}</h2>
             <h2>Departement :{x.department}</h2>
             <h2>Course :{x.course}</h2>
             <button onClick={()=>{handelUpdate(x.id)}}>Edit</button>
-            <button>Delete</button>
+            <button onClick={()=>{handelDelete(x.id)}}>Delete</button>
         </div>
     })}
     </>

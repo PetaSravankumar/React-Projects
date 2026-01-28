a22f6166da951093e26f5a964bd915a8daa2d256
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-01-28 15:06:26 +0530

    Initial commit - React projects

--- Student/src/pages/AddStudents.jsx
+++ Student/src/pages/AddStudents.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const AddStudents = () => {
   return (
-    <div>AddStudents</div>
+    <>
+    <form action="">
+        <input type="text" placeholder='Enter Student Name' required/><br />
+        <input type="email" placeholder='Enter Student Email' required/><br />
+        <input type="text"  placeholder='Enter Department' required/><br />
+        <input type="text" placeholder='College Name' required/><br />
+    </form>
+    </>
   )
 }
 

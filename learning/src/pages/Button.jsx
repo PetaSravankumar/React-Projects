b0a88f9be4e6ee50104acf57e9bf362bedbecb60
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-02-08 11:31:47 +0530

    Initial commit - React projects

--- learning/src/pages/Button.jsx
+++ learning/src/pages/Button.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Button = () => {
     function handelevent(){
-        console.log("hlo every one");
+        console.log("helloe every one");
+        
         
     }
   return (

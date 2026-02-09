d26702f3bc5671244e6afd282fcfd8d8d0cf7e65
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-02-09 12:49:04 +0530

    all push into the github

---
+++ colorpallate/src/color.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const color = () => {
+  return (
+    <div>color</div>
+  )
+}
+
+export default color
---
+++ colorpallate/src/pages/ColorPicker.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ColorPicker = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default ColorPicker

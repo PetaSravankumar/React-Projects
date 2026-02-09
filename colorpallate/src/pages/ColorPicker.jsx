238f4a5a2778f55e19ac8fbc327eec3a20128140
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-02-09 16:16:27 +0530

    all push into the github

--- colorpallate/src/pages/ColorPicker.jsx
+++ colorpallate/src/pages/ColorPicker.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 
 const ColorPicker = () => {
-  const [color,setcolor]=useState("#fffffff")
+  const [color,setcolor]=useState("#ffffff")
   function handelcolor(event){
-    setcolor(event)
+    setcolor(event.target.value)
   }
   return (
    <div className="colorpicker-container" >
     <h1>Pick a color</h1>
     <div className="colorpicker" style={{backgroundColor:color}}>
-      <p>Selected color </p>
+      <p>Selected color :{color} </p>
     </div>
     <label >Select  a color</label>
     <input type="color" value={color} onChange={handelcolor} />

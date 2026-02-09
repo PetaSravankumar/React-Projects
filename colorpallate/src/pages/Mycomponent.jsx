646ef6996c581f4ae19960e83114dfa4055e3a2b
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-02-09 23:00:01 +0530

    all push into the github

--- colorpallate/src/pages/Mycomponent.jsx
+++ colorpallate/src/pages/Mycomponent.jsx
@@ -6,15 +6,20 @@ const Mycomponent = () => {
                                   model:"Nano"
     })
     function handelyear(event){
-      setcar(event.target.value)
+      setcar(c=>({...c,year:event.target.value}))
     }
     function handelmaker(event){
-      setcar({...car,year:event.target.value})
+      setcar(c=>({...c,maker:event.target.value}))
+    }
+    function handelmodel(event){
+      setcar(c=>({...c,model:event.target.value}))
     }
   return (
     <>
-    <p>Your favort car ${car.year} ${car.maker} ${car.model}</p>
-    <input type="number" value={year} onChange={} />
+    <p>Your favort car {car.year} {car.maker} {car.model}</p>
+    <input type="number" value={car.year} onChange={handelyear} />
+    <input type="text" value={car.maker} onChange={handelmaker} />
+    <input type="text" value={car.model} onChange={handelmodel} />
     </>
   )
 }

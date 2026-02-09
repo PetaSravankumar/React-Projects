6910cbccf33c4845068a52e31c0b48a556ee316e
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-02-09 22:35:55 +0530

    all push into the github

--- colorpallate/src/pages/Mycomponent.jsx
+++ colorpallate/src/pages/Mycomponent.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Mycomponent = () => {
-    
+    const [car,setcar]=useState({year:2024,
+                                  maker:"Tata",
+                                  model:"Nano"
+    })
+    function handelyear(event){
+      setcar(event.target.value)
+    }
+    function handelmaker(event){
+      setcar({...car,year:event.target.value})
+    }
   return (
     <>
+    <p>Your favort car ${car.year} ${car.maker} ${car.model}</p>
+    <input type="number" value={year} onChange={} />
     </>
   )
 }

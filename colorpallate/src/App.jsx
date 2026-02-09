58aaf96e2697c4d6ab5901a562ad38058d981734
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-02-09 22:25:09 +0530

    all push into the github

--- colorpallate/src/App.jsx
+++ colorpallate/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ColorPicker from "./pages/ColorPicker"
+import Mycomponent from './pages/Mycomponent'
 const App = () => {
   return (
     <>
-    <ColorPicker/>/
+    <ColorPicker/>
+    <Mycomponent/>
     </>
   )
 }
---
+++ colorpallate/src/pages/Mycomponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mycomponent = () => {
+    
+  return (
+    <>
+    </>
+  )
+}
+
+export default Mycomponent

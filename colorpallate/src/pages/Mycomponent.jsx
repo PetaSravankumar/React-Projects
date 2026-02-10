231e434f38a178c5ba4faed9c549d82704d13374
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-02-10 10:48:45 +0530

    all push into the github

--- colorpallate/src/pages/Mycomponent.jsx
+++ colorpallate/src/pages/Mycomponent.jsx
@@ -1,26 +1,15 @@
 import React, { useState } from 'react'
 
 const Mycomponent = () => {
-    const [car,setcar]=useState({year:2024,
-                                  maker:"Tata",
-                                  model:"Nano"
-    })
-    function handelyear(event){
-      setcar(c=>({...c,year:event.target.value}))
-    }
-    function handelmaker(event){
-      setcar(c=>({...c,maker:event.target.value}))
-    }
-    function handelmodel(event){
-      setcar(c=>({...c,model:event.target.value}))
-    }
+  const [food,setfood]=useState(["apple","orange","Banana"]);
+  
   return (
-    <>
-    <p>Your favort car {car.year} {car.maker} {car.model}</p>
-    <input type="number" value={car.year} onChange={handelyear} />
-    <input type="text" value={car.maker} onChange={handelmaker} />
-    <input type="text" value={car.model} onChange={handelmodel} />
-    </>
+    <div>
+      <h2>List of fruits</h2>
+      <ul>
+        {food.map((items,index)=><li key={index}>{items}</li>)}
+      </ul>
+    </div>
   )
 }
 

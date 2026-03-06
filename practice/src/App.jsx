d5e886bbd805b2804647e4b233d320e9d31e1716
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-03-06 12:36:44 +0530

    add all the files

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -3,16 +3,22 @@ import React, { useState } from 'react'
 const App = () => {
   const [state,setState]=useState(false)
   function changeState(){
-    if(!state){
-      setState(true)
-    }
-    else{
-      setState(false)
-    }
+    setState(!state)
+  }
+  const buttonStyle = {
+    padding: '10px 20px',
+    fontSize: '20px',
+    cursor: 'pointer',
+    borderRadius: '25px',
+    border: 'none',
+    transition: '0.3s', // Makes the color change smooth
+    backgroundColor: state ? '#4CAF50' : '#ccc', // Green if ON, Gray if OFF
+    color: 'white',
+    width: '100px'
   }
   return (
     <>
-    <button onClick={changeState}>{state}</button>
+    <button onClick={changeState} style={buttonStyle}>{state ? "ON" : "OFF" }</button>
     </>
   )
 }

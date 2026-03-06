c779762c524a2dc016eec01d1e5948c32910a2fc
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-03-06 12:30:18 +0530

    add all the files

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [text,setText]=useState("")
-  function countChar(event){
-    setText(event.target.value) 
+  const [state,setState]=useState(false)
+  function changeState(){
+    if(!state){
+      setState(true)
+    }
+    else{
+      setState(false)
+    }
   }
-  
-
   return (
     <>
-    <input type="text" required placeholder='enter the input' value={text} onChange={countChar} />
-    <h2 >{text}</h2>
-    <h2>{text.length}</h2>
-   
+    <button onClick={changeState}>{state}</button>
     </>
   )
 }

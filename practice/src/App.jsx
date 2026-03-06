ce82f3e3891935e6e46bd9b8f5785157ff3b9cdf
Peta Sravan Kumar <sravankumarpeta5@gmail.com>
2026-03-06 12:13:13 +0530

    add all the files

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,22 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const App = () => {
-  const [count,setcount]=useState(0)
- 
-  function addition(){
-    setcount(count+1)
-  }
-  function dec(){
-    setcount(count-1)
-  }
   return (
-    <>
-    <p>count</p>
-    <p>{count}</p>
-    <button onClick={addition}>+</button>
-    <button onClick={dec}>-</button>
-
-    </>
+    <div>App</div>
   )
 }
 
